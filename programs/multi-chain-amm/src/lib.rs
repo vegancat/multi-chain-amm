9d41d713f8732910ef2d9c89d03c3e4a82814f02
Vegan Cat <mmostafavi247@gmail.com>
2022-12-14 22:27:23 +0330

    add frontend

--- programs/multi-chain-amm/src/lib.rs
+++ programs/multi-chain-amm/src/lib.rs
@@ -6,10 +6,31 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod multi_chain_amm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_liquidity_acc(ctx: Context<CreateLiquidityAcc>) -> Result<()> {
+        let liquidity_acc = &mut ctx.accounts.liquidity_acc;
+        let bump = *ctx.bumps.get("liquidity-account").unwrap();
+
+        liquidity_acc.eth = 0;
+        liquidity_acc.sol = 0;
+        liquidity_acc.usdc = 0;
+        liquidity_acc.bump = bump;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateLiquidityAcc<'info> {
+    #[account(init, payer = user, space = 8 + 1, seeds = [b"liquidity-account", user.key().as_ref()], bump)]
+    pub liquidity_acc: Account<'info, LiquidityAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct LiquidityAccount {
+    pub bump: u8,
+    pub eth: u64,
+    pub sol: u64,
+    pub usdc: u64,
+}
