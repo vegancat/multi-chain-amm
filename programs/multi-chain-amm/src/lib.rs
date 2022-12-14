620cd1538a9871f2da92b0ca165ea80105f65b7c
Vegan Cat <mmostafavi247@gmail.com>
2022-12-15 03:17:46 +0330

    add init_amm

--- programs/multi-chain-amm/src/lib.rs
+++ programs/multi-chain-amm/src/lib.rs
@@ -1,5 +1,8 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, initialize_account , InitializeAccount,Mint, Burn, MintTo, TokenAccount, Transfer, initialize_mint};
+use anchor_spl::token::{
+    self, initialize_account, initialize_mint, Burn, InitializeAccount, Mint, MintTo, TokenAccount,
+    Transfer,
+};
 use anchor_spl::{self};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
@@ -7,6 +10,37 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod multi_chain_amm {
     use super::*;
+
+    pub fn init_amm(ctx: Context<InitAmm>, fee: u16) -> Result<()> {
+        let pool = &mut ctx.accounts.pool_authority;
+        let token_program = ctx.accounts.token_program.to_account_info();
+        let eth_token_mint = &ctx.accounts.eth_token_mint;
+        let eth_token_acc = &ctx.accounts.eth_token_acc;
+        let rent = ctx.accounts.rent.to_account_info();
+        let bump = *ctx.bumps.get("pool").unwrap();
+
+        let create_acc_accounts = InitializeAccount {
+            account: eth_token_acc.to_account_info(),
+            mint: eth_token_mint.to_account_info(),
+            authority: pool.to_account_info(),
+            rent: rent.to_account_info(),
+        };
+
+        initialize_account(CpiContext::new_with_signer(
+            token_program,
+            create_acc_accounts,
+            &[&[b"pool", &[bump]]],
+        ))?;
+
+        pool.bump = bump;
+        pool.eth_value = 0;
+        pool.sol_value = 0;
+        pool.lp_value = 0;
+        pool.eth_token_account = eth_token_acc.key();
+        pool.fee = fee;
+
+        Ok(())
+    }
 
     pub fn create_liquidity_acc(ctx: Context<CreateLiquidityAcc>) -> Result<()> {
         let liquidity_acc = &mut ctx.accounts.liquidity_acc;
@@ -26,7 +60,7 @@ pub mod multi_chain_amm {
         initialize_account(CpiContext::new_with_signer(
             token_program,
             create_acc_accounts,
-            &[&[b"liquidity-account",user.key().as_ref()]]
+            &[&[b"liquidity-account", user.key().as_ref(), &[bump]]],
         ))?;
 
         liquidity_acc.eth = 0;
@@ -34,6 +68,42 @@ pub mod multi_chain_amm {
         liquidity_acc.bump = bump;
         Ok(())
     }
+}
+
+#[derive(Accounts)]
+pub struct InitAmm<'info> {
+    #[account(init, payer = user, space = 8 + 1 + 8 + 8 + 8 + 32 + 2, seeds = [b"pool"], bump)]
+    pub pool_authority: Account<'info, InitAmmAccount>,
+    pub eth_token_acc: Account<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    /// CHECK: checked inside the instruct
+    pub token_program: AccountInfo<'info>,
+    /// CHECK: checked inside the instruct
+    pub eth_token_mint: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct CreateLiquidityToken<'info> {
+    pub pool_account: Account<'info, InitAmmAccount>,
+    #[account(
+        init,
+        seeds = [b"lp-mint".as_ref()],
+        bump,
+        payer = user,
+        mint::decimals = 0,
+        mint::authority = pool_account
+    )]
+    pub lp_mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub eth_token_acc: Account<'info, TokenAccount>,
+    pub eth_token_mint: AccountInfo<'info>,
+    pub token_program: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
 }
 
 #[derive(Accounts)]
@@ -49,15 +119,6 @@ pub struct CreateLiquidityAcc<'info> {
     pub eth_token_acc: Account<'info, TokenAccount>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
-    #[account(
-        init, 
-        seeds = [b"mint".as_ref()], 
-        bump,
-        payer = user,
-        mint::decimals = 0, 
-        mint::authority = lp_mint
-    )]
-    pub lp_mint: Account<'info, Mint>,
 }
 
 #[account]
@@ -65,4 +126,14 @@ pub struct LiquidityAccount {
     pub bump: u8,
     pub eth: u64,
     pub sol: u64,
+}
+
+#[account]
+pub struct InitAmmAccount {
+    bump: u8,
+    eth_value: u64,
+    sol_value: u64,
+    lp_value: u64,
+    eth_token_account: Pubkey,
+    fee: u16,
 }
